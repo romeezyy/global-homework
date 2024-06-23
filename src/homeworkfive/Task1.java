0a91447c25ac4a3d9845b289794a409c6c02dbb3
Roman Gurov <romario.gurov@gmail.com>
2024-06-23 21:16:38 +0300

    It's task1(исправил)

--- src/homeworkfive/Task1.java
+++ src/homeworkfive/Task1.java
@@ -2,6 +2,6 @@ package homeworkfive;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello keysik world");
     }
 }
